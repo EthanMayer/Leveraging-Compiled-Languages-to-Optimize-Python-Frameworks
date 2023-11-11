c14cca9bbe137c0bab3d3e185646fc875bdad275
Ethan Mayer <ethan.mayer@comcast.net>
2023-11-11 17:58:04 -0600

    Comment thread C++ file

--- threadBody.cpp
+++ threadBody.cpp
@@ -1,16 +1,18 @@
 /*
 C++ Thread Body File
-Created on Sept. 21, 2023
+Contains the thread body functions for C++ pthreads
 
 @author: EthanMayer
 */
 
+// Includes
 #include <pthread.h>
 #include <iostream>
 #include <zmq.hpp>
 #include <unistd.h>
 #include "json.hpp"
 
+// Struct for containing pthread parameter data
 typedef struct {
     std::string addr;
     int runs;
@@ -19,6 +21,7 @@ typedef struct {
     int root;
 } arg_array;
 
+// Iterative Fibonacci function implementation for C++
 int fib(int n) {
     if (n == 0) { return 0; }
     if (n == 1) { return 1; }
@@ -34,6 +37,7 @@ int fib(int n) {
     return curNum;
 }
 
+// Recursive Fibonacci function implementation for C++
 int fib_recursive(int n) {
     if (n == 0) { return 0; }
     if (n == 1) { return 1; }
@@ -41,31 +45,34 @@ int fib_recursive(int n) {
     return (fib_recursive(n - 2) + fib_recursive(n - 1));
 }
 
+// Memory allocation function implementation for C++
 void memory_alloc() {
     int* ptrs[100];
 
+    // Allocate a size 100 array of 4096 bytes
     for (int i = 0; i < 100; i++) {
         ptrs[i] = (int*)malloc(4096);
     }
 
+    // Free the memory allocations
     for (int i = 0; i < 100; i++) {
         free(ptrs[i]);
     }
 }
 
+// Thread body function to run tests
+// Extern C to avoid C++ name mangling so it can be loaded from .so
 extern "C" void* thread1(arg_array arg) {
     pthread_t tid = pthread_self();
     if (arg.print) { std::cout << "Thread: Process ID: " << getpid() << " Thread ID: " << tid << std::endl; }
     if (arg.print) { std::cout << "Thread: arg.addr: " << arg.addr << " arg.runs: " << arg.runs << std::endl; }
 
-    // CppComponent testComp(NULL);
-
-    // std::cout << "Test Component Name: " << testComp.getName() << std::endl;
-
+    // Setup ZMQ PAIR socket to communicate with Python main thread
     zmq::context_t ctx;
     zmq::socket_t sock(ctx, ZMQ_PAIR);
     sock.connect(arg.addr);
 
+    // Forward declaration of all message variables
     int i = 0;
     nlohmann::json send;
     std::string send_str;
@@ -73,15 +80,22 @@ extern "C" void* thread1(arg_array arg) {
     nlohmann::json reply_json;
     int x = 0;
 
+    // Test run loop
     while (i < arg.runs) {
 
+        // If no work, just send back simple JSON string message
         if (arg.test_type == 0) {
             nlohmann::json send_no_fib;
             send_no_fib["Contents"] = "Ready";
             send = send_no_fib;
+
         } else if (arg.test_type == 1) {
+
+            // If light math work, send iterative Fibonacci of square root of message number
             if (arg.root) {
                 x = fib((int)std::sqrt(i));
+
+            // If heavy math work, send iterative Fibonacci of message number
             } else {
                 x = fib((int)i);
             }
@@ -89,9 +103,14 @@ extern "C" void* thread1(arg_array arg) {
             nlohmann::json send_fib;
             send_fib[send_fib_str] = x;
             send = send_fib;
+
         } else if (arg.test_type == 2) {
+
+            // If light function call work, send recursive Fibonacci of square root of message number
             if (arg.root) {
                 x = fib_recursive((int)std::sqrt(i));
+
+            // If heavy function call work, send recursive Fibonacci of message number
             } else {
                 x = fib_recursive((int)i);
             }
@@ -99,6 +118,8 @@ extern "C" void* thread1(arg_array arg) {
             nlohmann::json send_recur_fib;
             send_recur_fib[send_fib_str] = x;
             send = send_recur_fib;
+
+        // If memory allocation work, allocate memory before sending message back
         } else if (arg.test_type == 3) {
             memory_alloc();
             nlohmann::json send_mem;
@@ -106,23 +127,28 @@ extern "C" void* thread1(arg_array arg) {
             send = send_mem;
         }
         
+        // Send message to main thread
         send_str = send.dump();
         if (arg.print) { std::cout << "Thread: sending: " << send_str << std::endl; }
         zmq::message_t query(send_str.length());
         std::memcpy(query.data(), (send_str.c_str()), (send_str.size()));
         sock.send(query, zmq::send_flags::none);
 
+        // Receive message from main thread
         zmq::recv_result_t e = sock.recv(reply, zmq::recv_flags::none);
         reply_json = nlohmann::json::parse(reply.to_string());
         if (arg.print) { std::cout << "Thread: reply received: \n" << reply_json.dump(4) << std::endl; }
         if (arg.print) { std::cout << "Value of Point: " << reply_json["__value__"]["values"][1] << std::endl; }
+
         i = (int)reply_json["__value__"]["values"][1];
     }
     
+    // Close ZMQ socket and context
     sock.close();
     ctx.close();
 
     if (arg.print) { std::cout << "Thread: Exiting" << std::endl; }
 
+    // Exit thread
     pthread_exit(NULL);
 }
